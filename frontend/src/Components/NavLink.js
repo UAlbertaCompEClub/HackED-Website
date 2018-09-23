9fad2da318daff6159e2bd36903939395ac1e1be
Mohammad Al-Ahdal <mohammad@enggmomo.me>
2018-09-23 14:18:29 -0600

    add hoverability to links

--- frontend/src/Components/NavLink.js
+++ frontend/src/Components/NavLink.js
@@ -2,19 +2,37 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 
 export default class NavLink extends React.Component {
+    state = {
+
+    }
     render() {
         return (
             <div>
-                <Link style={
-                    {
-                        fontFamily: "Open Sans",
-                        fontWeight: "500",
-                        fontSize: 12,
-                        textDecoration: "none",
-                        color: this.props.style ? this.props.style.color : this.props.light ? "white" : "black",
-                        ...this.props.style
-                    }
-                } to={this.props.href ? this.props.href : undefined}>
+                <Link
+                    //has props hoverStyle
+                    onMouseEnter={() => {
+                        if (this.props.onMouseEnter) {
+                            this.props.onMouseEnter();
+                        }
+                        this.setState({ hoverStyle: this.props.hoverStyle })
+                    }}
+                    onMouseLeave={() => {
+                        if (this.props.onMouseLeave) {
+                            this.props.onMouseLeave();
+                        }
+                        this.setState({ hoverStyle: undefined })
+                    }}
+                    style={
+                        {
+                            fontFamily: "Open Sans",
+                            fontWeight: "500",
+                            fontSize: 12,
+                            textDecoration: "none",
+                            color: this.props.style ? this.props.style.color : this.props.light ? "white" : "black",
+                            ...this.props.style,
+                            ...this.state.hoverStyle
+                        }
+                    } to={this.props.href ? this.props.href : undefined}>
                     {this.props.children}
                 </Link>
             </div>
